f6a28a443bd487f76dfecb113a3e04682f4c9974
JackDing <361164217@qq.com>
2020-02-03 18:31:19 +0800

    基础功能实现

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,71 @@
-const URL = "https://api.worldtradingdata.com/api/v1/stock"
+import * as https from "https"
+import * as querystring from "querystring"
+import {token} from "./token"
 
-const Grid = require("console-grid");
-const grid = new Grid();
+const URL = "https://api.worldtradingdata.com/api/v1/stock?"
+const Grid = require("console-grid")
+const grid = new Grid()
 
 
-const printData = (data:any): void => {
-	const renderData = {
-		option: {
-			sortField: "name"
-		},
-		columns: [{
-			id: "name",
-			name: "Name",
-			type: "string",
-			maxWidth: 38
-		}, {
-			id: "value",
-			name: "Value",
-			type: "string",
-			maxWidth: 7
-		}],
-		rows: [{
-			name: data,
-			value: "1"
-		}]
-	}
-	grid.render(renderData);
+export const queryData = (name: string) => {
+
+  const queryParam = querystring.stringify({
+    api_token: token,
+    symbol: name
+  })
+  const url = URL + queryParam
+  https.get(url, (res) => {
+    //返回数据可能是分段的
+    let chunks: any[] = []
+    res.on("data", (chunk) => {
+      chunks.push(chunk)
+    })
+    res.on("end", () => {
+      const result = JSON.parse(Buffer.concat(chunks).toString())
+      if (result.Message) {
+        console.error(result.Message)
+      } else {
+        printData(result.data[0])
+      }
+    })
+  }).on("error", (e) => {
+    console.error(e)
+  })
+
 }
 
-const getData = ()=>{
 
+export const printData = (data: any): void => {
+  // console.log(data)
+  const renderData = {
+    option: {
+      sortField: "name"
+    },
+    columns: [
+      {
+        id: "name",
+        name: "股票名称",
+        type: "string",
+        maxWidth: 38
+      },
+      {
+        id: "value",
+        name: "当前股价",
+        type: "string",
+        maxWidth: 10
+      },
+      {
+        id: "cap",
+        name: "当前市值",
+        type: "string",
+        maxWidth: 10
+      },
+    ],
+    rows: [{
+      name: data.name,
+      value: data.price,
+      cap:((data.market_cap-0)/100000000).toFixed(2)
+    }]
+  }
+  grid.render(renderData)
 }
-
-
-export default printData
-
-
--- src/token.ts
+++ src/token.ts
@@ -1,3 +1,3 @@
-const token = "JFG3udznzsBaOVCjGoOgHNzhKnkLGhpXhmMwya1tYYTATnQ22j4vJ7kCriKs"
+export const token = "JFG3udznzsBaOVCjGoOgHNzhKnkLGhpXhmMwya1tYYTATnQ22j4vJ7kCriKs"
 
-export default token
+
